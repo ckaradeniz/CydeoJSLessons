7d7af66a9d5391b232c34f9cf493dc217621a69e
OscarSah <oscar@cybertekschool.com>
2023-04-29 18:45:52 +0300

    add: loops

---
+++ 04_Loops.js
@@ -0,0 +1,14 @@
+// loops are same with Java: for loop, while loop, do while 
+for (let i = 0; i < 5 ; i++){  // local scope
+    console.log(i);
+}
+// console.log(i);
+
+// old type of declaring variables before ES6 : side topic
+for(var j = 0; j < 5 ; j++){
+    console.log(j);
+}
+console.log(j);  // this type is global and function scope
+
+
+
