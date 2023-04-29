4c46b6c3d7e9405e6e7dfdccc0e54b2286b5465d
OscarSah <oscar@cybertekschool.com>
2023-04-29 17:50:46 +0300

    add: comparison

---
+++ 03_Operators.js
@@ -0,0 +1,13 @@
+// Order of operators in JS is same with JAVA
+console.log(4+4*(-1)-8/2); 
+// assignment operators
+let x = 5;
+x+=5;
+console.log(x);
+
+// comparison: similar to JAVA with some difference
+
+let one = 1;
+let one_again = 1;
+let one_string = '1';
+let two_string = '2';
