34c398952c98643c590cfdf94945e4e5def8e57d
OscarSah <oscar@cybertekschool.com>
2023-04-29 18:49:07 +0300

    add: loops

--- 04_Loops.js
+++ 04_Loops.js
@@ -10,5 +10,15 @@ for(var j = 0; j < 5 ; j++){
 }
 console.log(j);  // this type is global and function scope
 
+let n1 = 0;
+while (n1 < 3) {
+    console.log(n1);
+    n1++;
+}
 
+let n2 = 0;
+do {
+    console.log(n2);
+    n2++;
+} while (n2 < 3);
 
