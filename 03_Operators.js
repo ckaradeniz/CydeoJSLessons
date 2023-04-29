ef53de2814f6baa9f0ac8c0ee9d6b56178670158
OscarSah <oscar@cybertekschool.com>
2023-04-29 18:00:31 +0300

    add: logical

--- 03_Operators.js
+++ 03_Operators.js
@@ -5,9 +5,20 @@ let x = 5;
 x+=5;
 console.log(x);
 
-// comparison: similar to JAVA with some difference
+// comparison: similar to JAVA with some difference: == and === signs
 
 let one = 1;
 let one_again = 1;
 let one_string = '1';
 let two_string = '2';
+
+console.log(one == one_again); // true
+console.log(one == one_string); // true: == sign is looking for value equality
+console.log(one === one_string); // false : looking for both value and dataType
+console.log(one_string === two_string); // false
+
+
+// logical operators sama like JAVA: you have only short circuit && , || 
+console.log((5<2)&&(2<5)); // false
+
+
