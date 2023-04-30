92ccddcea1942047e2c3fb383d7f6ae1d718598d
OscarSah <oscar@cybertekschool.com>
2023-04-30 17:05:46 +0300

    add: functions async nature of JS

--- 06_Functions.js
+++ 06_Functions.js
@@ -49,4 +49,4 @@ let aveResult = average(10,20,sum);
 console.log(aveResult);
 
 aveResult = average(10,20,multiply);
-console.log(aveResult);
+console.log("average result from multiplication : "+aveResult);
